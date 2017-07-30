53aea220352e555102f2d2b35649f2916435d3fb
BlackKite0206233 <samuel991640@gmail.com>
2017-07-30 11:02:20 +0800

    init project

---
+++ src/components/FileInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="fileInput">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'fileInput',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
