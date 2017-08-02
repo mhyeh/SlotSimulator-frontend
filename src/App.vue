aa070b237fa45b4b088a119a96200aeab32b5a26
BlackKite0206233 <samuel991640@gmail.com>
2017-08-02 16:16:45 +0800

    add theme

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,65 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app id="app" standalone>
+    <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar tag="div">
+            <v-list-tile-avatar>
+              <v-icon>mdi-drawing</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>select chart</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon @click.native.stop="mini = !mini" ripple>
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed class="indigo darken-4" dark>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Chart Generator</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { title: 'RTP', icon: 'mdi-chart-bar' },
+        { title: 'Total Net Win', icon: 'mdi-chart-line' },
+        { title: 'Survival Rate', icon: 'mdi-table-large' }
+      ],
+      mini: false,
+      right: null
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../node_modules/mdi/css/materialdesignicons.min.css';
 </style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,62 +1,16 @@
 <template>
   <div id="mainPage">
-    <file-input v-on:readFile="parseToJson"></file-input>
-    <line-chart v-if="hasFile"></line-chart>
   </div>
 </template>
 
 <script>
-  let papaparse = require('papaparse')
-
-  import LineChart from '@/components/LineChart'
-  import FileInput from '@/components/FileInput'
-
   export default {
     name: 'mainPage',
-    data () {
-      return {
-        hasFile: false,
-        fileData: {}
-      }
-    },
-    methods: {
-      parseToJson (data) {
-        this.hasFile = true
-
-        let self = this
-
-        let config = {
-          complete: function (result) {
-            self.fileData = result.data
-            console.log('Finish')
-          }
-        }
-
-        papaparse.parse(data, config)
-
-        // draw chart
-      }
-    },
-    components: {LineChart, FileInput}
   }
 </script>
 
 <style scoped>
-  #mainPage {
-    top: 10%;
-    left: 10%;
-    height: 80%;
-    width: 80%;
-    text-align: center;
-  }
 
-  file-input {
-    height: 20%;
-  }
-
-  line-chart {
-    height: 80%;
-  }
 </style>
 
 
