f182c760e8fc0300aaba9d1486a955527fd58c1a
blackkite0206233 <samuel991640@gmail.com>
2017-08-24 17:39:33 +0800

    fix some bugs

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -67,7 +67,7 @@ module.exports = {
         options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
-          publicPath: utils.assetsPath('../../fonts/[name].[hash:7].[ext]')
+          publicPath: utils.assetsPath('../../../')
         }
       }
     ]
